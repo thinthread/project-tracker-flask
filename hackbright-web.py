b31ee99943b38f8406b9cf0be2fad31b549271d4
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-25 13:01:15 -0800

    Part 2 sort of completed

--- hackbright-web.py
+++ hackbright-web.py
@@ -11,9 +11,14 @@ def get_student():
 
     github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
+
+    rows= hackbright.get_grades_by_github(github)
+    print rows
+
     html = render_template("student_info.html",first=first,
                                                last=last,
-                                               github=github)
+                                               github=github,
+                                               rows=rows)
 
     return html
 
@@ -25,15 +30,24 @@ def get_student_form():
     return render_template("student_search.html")
 
 
+@app.route("/student-add-form")
+def student_add_form():
+    """Displays form to add student."""
+
+    return render_template("student_form.html")
+
 @app.route("/student-add", methods=['POST'])
 def student_add():
     """Add a student."""
     first = request.form.get("first")
+    print first
     last = request.form.get("last")
+    print last
     github = request.form.get("github")
+    print github
     hackbright.make_new_student(first, last, github)
 
-    return render_template("student_form.html")
+    return render_template("student_response.html")
 
 
 if __name__ == "__main__":
